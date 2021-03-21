061e2d8d35964cd0a8e3246bf34cf83f55efc835
ortango <8408464+ortango@users.noreply.github.com>
2021-03-21 08:39:30 -0400

    revert ignorenew.sh index fix, but add a third * to match

--- commands/ignorenew.sh
+++ commands/ignorenew.sh
@@ -2,7 +2,7 @@
 #set temporary rule to ignore all new windows
 #requires notification daemon
 
-i=$(bspc rule -l | grep -n '\*:\*:\* => focus=off' | cut -d: -f1)
+i=$(bspc rule -l | sed -n '/\*:\*:\* => focus=off/=')
 if [ -n "$i" ]; then
     bspc rule -r "^${i}" &&
     notify-send "bspwm" "no longer ignoring new windows"
