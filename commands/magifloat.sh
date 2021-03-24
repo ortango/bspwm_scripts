5b3f547824c1ba97c49b7f27d8aa17eda4dd54a0
ortango <8408464+ortango@users.noreply.github.com>
2021-03-24 01:30:20 -0400

    changed magifloat to use a window atom rather then a file

--- commands/magifloat.sh
+++ commands/magifloat.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 #args [nodeid]
-#requires jq
+#requires wmutils, jq
 
-tmpdir='/tmp/bspwm/'
-totile(){
-    r="$(cat "$file")"
-    rm "$file"
-    bspc node "$1" -n "$r" -t tiled
+atom_name="BSPWM_RECP"
+_atomx(){
+    declare -a cmd
+    cmd=([0]=atomx [2]="$atom_name")
+    [[ "$1" == '-d' ]] && {
+        cmd[1]=-d
+        shift
+    }
+    [[ -n "$2" ]] && cmd[2]="${cmd[2]}=${2}"
+    "${cmd[@]}" "$(printf '0x%08x' "$1")"
 }
+bnq(){ bspc query -N -n "$1" >/dev/null; }
 
-n=$(bspc query -N -n "${1:-focused}") || exit 1
-file="${tmpdir}/${n}.magicfloat"
-if [[ -f "$file" ]]; then
-    bspc query -N -n "${n}.floating" &&
-    totile "$n"
+n="$(bspc query -N -n "${1:-focused}")" || exit 1
+r="$(_atomx "$n")"
+if [[ -n "$r" ]]; then
+    if bnq "$n"; then
+        if bnq "$r"; then
+            bspc node "$n" -n "$r" -t tiled
+        else
+            bspc node "$n" -t tiled
+        fi
+        _atomx -d "$n"
+    fi
 else
-    mapfile -t g < <(bspc query -T -n "$n" | jq -r '.client | .tiledRectangle[], .floatingRectangle[]')
-    [[ "${g[*]}" ]] && mkdir -p "$tmpdir" || exit 1
-    for i in {0..3}; do
-        (( g[i] -= g[i+4] ))
-    done
-    bspc query -N -n "${n}.!floating" &&
-    stateargs=(-t floating)
-    bspc node "$n" -i \
-        "${stateargs[@]}" \
-        -v "${g[0]}" "${g[1]}" \
-        -z bottom_right "${g[2]}" "${g[3]}"
-    bspc query -N -n "${n}#@brother.leaf.!window" > "$file"
+    if bnq "${n}.!floating"; then
+        bnq "${n}.fullscreen" && bspc node "$n" -t ~fullscreen
+        mapfile -t g < <(bspc query -T -n "$n" | jq -r \
+            '.client | .tiledRectangle[], .floatingRectangle[]')
+        [[ -n "${g[*]}" ]] || exit 1
+        for i in {0..3}; do
+            (( g[i] -= g[i+4] ))
+        done
+        bspc node "$n" -i -t floating \
+            -v "${g[0]}" "${g[1]}" \
+            -z bottom_right "${g[2]}" "${g[3]}"
+        r="$(bspc query -N -n "${n}#@brother.leaf.!window")" &&
+            _atomx "$n" "$r" >/dev/null
+     fi
 fi
